dcb0eefc14dd50fdba0ea7323917417acb874608
chase <human@gmail.com>
2022-07-07 11:07:49 -0700

    Help me

--- main.py
+++ main.py
@@ -17,48 +17,22 @@ ACC = 0.5
 FRIC = -0.12
 FPS = 240
 Score = 0
-black = (0,0,0)
 FramePerSec = pygame.time.Clock()
 
 clock = pygame.time.Clock()
 displaysurface = pygame.display.set_mode((720, 720))
 pygame.display.set_caption("Game")
 
-clock.tick(60)
-pygame.display.update()
-
 def scrollY(screenSurf, offsetY):
-    width, height = screenSurf.get_size()
+    WIDTH, HEIGHT = screenSurf.get_size()
     copySurf = screenSurf.copy()
     screenSurf.blit(copySurf, (0, offsetY))
-    if offsetY < 0:
-        screenSurf.blit(copySurf, (0, height + offsetY), (0, 0, width, -offsetY))
-    else:
-        screenSurf.blit(copySurf, (0, 0), (0, height - offsetY, width, offsetY))
-
-def text_objects(text, font):
-    textSurface = font.render(text, True, black)
-    return textSurface, textSurface.get_rect()
-
-def message_display(text):
-    largeText = pygame.font.Font('font.ttf',115)
-    TextSurf, TextRect = text_objects(text, largeText)
-    TextRect.center = ((WIDTH/2),(HEIGHT/2))
-    displaysurface.blit(TextSurf, TextRect)
-
-    pygame.display.update()
-
-    time.sleep(2)
-
 
 # GENERATING THE PLAYER
 playerSprite = pygame.image.load("playerSprite.png")
 class Player(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
-        # self.image = pygame.image.load("character.png")
-        #self.x = x
-        #self.y = y
         self.image = playerSprite
         self.rect = self.image.get_rect()
         self.pos = vec((360, 560))
@@ -71,9 +45,9 @@ class Player(pygame.sprite.Sprite):
 
         pressed_keys = pygame.key.get_pressed()
 
-        if pressed_keys[K_LEFT]:
+        if pressed_keys[K_LEFT] or pressed_keys[K_a]:
             self.acc.x = -ACC
-        if pressed_keys[K_RIGHT]:
+        if pressed_keys[K_RIGHT] or pressed_keys[K_d]:
             self.acc.x = ACC
 
         self.acc.x += self.vel.x * FRIC
@@ -93,10 +67,10 @@ class Player(pygame.sprite.Sprite):
         if hits:
             self.vel.y = -15
             scrollY(displaysurface, -50)
-            pygame.display.update()
+            #pygame.display.update()
             for entity in all_sprites:
                 displaysurface.blit(entity.image, entity.rect)
-                entity.pos += vec(0, -50)
+                entity.pos += vec(0, -100)
 
 
 # CHECKS FOR COLLISION UPDATES
@@ -134,8 +108,6 @@ class object(pygame.sprite.Sprite):
         pass
 
 
-
-
 randomX = random.randint(200, 420)
 randomY = random.randint(220, 300)
 randomX2 = random.randint(100, 220)
@@ -159,10 +131,23 @@ platforms.add(PT1)
 platforms.add(PT2)
 platforms.add(PT3)
 
-
-
-
+myfont = pygame.font.SysFont(None, 30)
+myfont2 = pygame.font.SysFont(None, 100)
 while True:
+    displaysurface.fill((0, 0, 0))
+    P1.update()
+    if abs(P1.pos[0] - C1.pos[0]) < 75 and abs(P1.pos[1] - C1.pos[1]) < 75:
+        Score += 1
+        print(Score)
+    print(P1.pos[0], P1.pos[1])
+    if P1.pos[1] > 720:
+        deathtext = myfont2.render('YOU DIED', True, (255, 100, 100))
+        displaysurface.blit(deathtext, (200, 360))
+
+    for entity in all_sprites:
+        displaysurface.blit(entity.image, entity.rect)
+        entity.move()
+
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
@@ -170,29 +155,12 @@ while True:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 P1.jump()
-
-
-
-    displaysurface.fill((0, 0, 0))
-    P1.update()
-    if abs(P1.pos[0] - C1.pos[0]) < 75 and abs(P1.pos[1] - C1.pos[1]) < 75:
-        Score += 1
-        print(Score)
-    #print(P1.pos[0], P1.pos[1])
-    if P1.pos[1] > 720:
-        message_display("You Died")
-        time.sleep(5)
-        pygame.quit()
-
-
-
-    for entity in all_sprites:
-        displaysurface.blit(entity.image, entity.rect)
-        entity.move()
-
-
-
-
-
+# 1 charactar = 10
+    mytext = myfont.render('score: ' + str(Score), True, (255, 100, 100))
+    mytext2 = myfont.render('X: ' + str(P1.pos[0] // 1), True, (255, 100, 100))
+    mytext3 = myfont.render('Y: ' + str(P1.pos[1] // 1), True, (255, 100, 100))
+    displaysurface.blit(mytext, (610, 1))
+    displaysurface.blit(mytext2, (610, 20))
+    displaysurface.blit(mytext3, (610, 40))
     pygame.display.update()
     FramePerSec.tick(FPS)
